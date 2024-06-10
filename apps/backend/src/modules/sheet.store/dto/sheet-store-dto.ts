a35033f22c2378c196dd55f9c55abf313fa0050d
Innocent-akim <gentilakili98@gmail.com>
2024-06-10 19:57:10 +0200

    [add] module sheet store dto

---
+++ apps/backend/src/modules/sheet.store/dto/sheet-store-dto.ts
@@ -0,0 +1,35 @@
+import { IProducts, IStore } from "@uni/contracts";
+import { Type } from "class-transformer";
+import { IsDate, IsNotEmpty, IsString } from "class-validator";
+
+export class SheetStoreDto{
+    @IsNotEmpty()
+    @Type(()=>Number)
+    initial_stock: number;
+
+    @IsNotEmpty()
+    @Type(()=>Number)
+    out_stock: number;
+
+    @IsNotEmpty()
+    @Type(()=>Number)
+    in_stock: number;
+
+    @IsNotEmpty()
+    @Type(()=>Number)
+    end_stock: number;
+
+    @IsNotEmpty()
+    @IsDate()
+    currentDate: Date;
+
+
+    @IsNotEmpty()
+    @IsString()
+    productId:IProducts['id']
+
+    @IsNotEmpty()
+    @IsString()
+    storeId:IStore['id']
+
+}
