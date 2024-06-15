6153979a2763a486605262864aa0d1c83a808cc3
Innocent-akim <gentilakili98@gmail.com>
2024-06-15 12:33:53 +0200

    add function create new deposit

--- apps/backend/src/modules/deposit/service/deposit.service.ts
+++ apps/backend/src/modules/deposit/service/deposit.service.ts
@@ -3,6 +3,7 @@ import { CrudService } from '@uni/crud';
 import { DepositEntity } from '../entities/deposit.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TypeOrmDeposit } from '../repository/type-orm-deposit';
+import { DepositDto } from '../dto/deposit.dto';
 
 @Injectable()
 export class DepositService extends CrudService<DepositEntity> {
@@ -11,5 +12,11 @@ export class DepositService extends CrudService<DepositEntity> {
          typeOrmDeposit:TypeOrmDeposit
     ){
         super(typeOrmDeposit)
+    }
+
+    
+    async createDeposit(deposit:DepositDto):Promise<DepositEntity>{
+        const responsive=await this.create(deposit);
+        return responsive
     }
 }
