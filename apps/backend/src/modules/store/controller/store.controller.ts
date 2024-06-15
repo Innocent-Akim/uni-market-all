c1e23723fed74d1390db182c042144ef138216e2
Innocent-akim <gentilakili98@gmail.com>
2024-06-15 12:54:33 +0200

    add function create new store

--- apps/backend/src/modules/store/controller/store.controller.ts
+++ apps/backend/src/modules/store/controller/store.controller.ts
@@ -1,11 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { CrudController } from '@uni/crud';
 import { StoreService } from '../service/store.service';
 import { StoreEntity } from '../entities/store.entity';
+import { StoreDto } from '../dto/store-dto';
 
 @Controller()
 export class StoreController extends CrudController<StoreEntity> {
     constructor(private storeService: StoreService) {
         super(storeService)
+    }
+
+    @Post()
+    async createStor(@Body() store: StoreDto):Promise<StoreEntity>{
+        return await this.storeService.createStore(store);
     }
 }
