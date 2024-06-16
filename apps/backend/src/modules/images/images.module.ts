ff6b89b82289b8260776c51befed1ed1cc6b94d3
Innocent-akim <gentilakili98@gmail.com>
2024-06-16 09:31:36 +0200

    update module images

--- apps/backend/src/modules/images/images.module.ts
+++ apps/backend/src/modules/images/images.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ImagesController } from './controller/images/images.controller';
-import { ImagesService } from './service/images/images.service';
+import { ImagesController } from './controller/images.controller';
+import { ImagesService } from './service/images.service';
 
 @Module({
   controllers: [ImagesController],
