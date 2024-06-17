6d5690d0720f05353c5005c412288908df5c7418
Innocent-akim <gentilakili98@gmail.com>
2024-06-17 14:19:23 +0200

    feat: siderbar

--- apps/frontend/components/layouts/Siderbar.tsx
+++ apps/frontend/components/layouts/Siderbar.tsx
@@ -16,6 +16,7 @@ import { LuHelpCircle } from "react-icons/lu";
 import { IoFolderOpenOutline } from "react-icons/io5";
 import { GrPieChart } from "react-icons/gr";
 import { usePathname } from 'next/navigation';
+import { LINK_DASHBOARD, LINK_ENTREE, LINK_FINANCE, LINK_IDENTIFICATE, LINK_PARAMETRE, LINK_PRODUCT, LINK_RAPPORT, LINK_SORTIE } from '@/lib/linkNavigator';
 
 
 function Siderbar() {
@@ -64,22 +65,113 @@ function Siderbar() {
         };
     };
     const items: CollapseProps['items'] = [];
+    items.push(
+        RenderItem({
+            headerText: 'Tableau de Bord',
+            icon: <GrDashboard />,
+            links: LINK_DASHBOARD
+        })
+    );
+    items.push(
+        RenderItem({
+            headerText: 'Produicts',
+            icon: <MdOutlineBookmarkAdded />,
+            links: LINK_PRODUCT
+        })
+    );
+    items.push(
+        RenderItem({
+            headerText: 'Sorties',
+            icon: <MdAddShoppingCart />,
+            links: LINK_SORTIE
+        })
+    );
+    items.push(
+        RenderItem({
+            headerText: 'Entrées',
+            icon: <TfiShoppingCartFull />,
+            links: LINK_ENTREE
+        })
+    );
+    items.push(
+        RenderItem({
+            headerText: 'Finances',
+            icon: <GrPieChart />,
+            links: LINK_FINANCE
+        })
+    );
+    items.push(
+        RenderItem({
+            headerText: 'Rapports',
+            icon: <IoFolderOpenOutline />,
+            links: LINK_RAPPORT
+        })
+    );
+    items.push(
+        RenderItem({
+            headerText: 'Identifications',
+            icon: <BiUserPlus />,
+            links: LINK_IDENTIFICATE
+        })
+    );
+    items.push(
+        RenderItem({
+            headerText: 'Paramètres du système',
+            icon: <SlSettings />,
+            links: LINK_PARAMETRE
+        })
+    );
+
     return (
-        <aside
-            id='application-sidebar'
-            className='hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform hidden fixed top-0 left-0 bottom-0 z-[60] w-72 bg-white border-r border-r-transparent border-transparent pt-7 pb-10 overflow-y-auto scrollbar-y lg:block lg:translate-x-0 lg:right-auto lg:bottom-0 dark:scrollbar-y dark:bg-gray-800 dark:border-transparent '
-        >
-            <div className='px-6'>
-                <Link
-                    className='flex-none text-xl font-bold text-sky-500 dark:text-white'
-                    href='/'
-                    aria-label='Brand'>
-                    UNI-MARKET
-                </Link>
-            </div>
-            <AccordionMenu/>
-        </aside>
-    )
-}
+        <>
+            {/* Sidebar */}
+            <aside
+                id='application-sidebar'
+                className='hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform hidden fixed top-0 left-0 bottom-0 z-[60] w-60 bg-white border-r border-slate-50 pt-7 pb-10 overflow-y-auto scrollbar-y lg:block lg:translate-x-0 lg:right-auto lg:bottom-0 dark:scrollbar-y dark:bg-gray-800 dark:border-gray-700 '
+            >
+                <div className='px-6'>
+                    <Link
+                        className='flex-none text-xl font-bold text-sky-500 dark:text-white'
+                        href='/'
+                        aria-label='Brand'>
+                        UNI-MARKET
+                    </Link>
+                </div>
+                <Collapse
+                    ghost={true}
+                    accordion
+                    items={items}
+                    bordered={true}
+                    size='small'
+                />
+                <div className='text-[#737a87] text-[14.3px] mx-3 my-3'>
+                    <Link
+                        href='/soccase'
+                        className='flex gap-2 items-center hover:text-secondary-800 duration-150'
+                    >
+                        <LuBellRing className='text-xl' />
+                        Alertes de Stock
+                    </Link>
+                </div>
+                <div className='text-[#737a87] text-[14.3px] mx-3 my-3'>
+                    <Link
+                        href='/soccase'
+                        className='flex gap-2 items-center hover:text-secondary-800 duration-150'
+                    >
+                        <LuHelpCircle className='text-xl' />
+                        Aide et Documentation
+                    </Link>
+                </div>
+                <nav
+                    className='hs-accordion-group p-6 w-full flex flex-col flex-wrap'
+                    data-hs-accordion-always-open=''
+                >
+
+                </nav>
+            </aside>
+            {/* End Sidebar */}
+        </>
+    );
+};
 
 export default Siderbar
