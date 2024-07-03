6a4908057a7d6a9de6eb4c5e6babf47eb6f1db42
Innocent-akim <gentilakili98@gmail.com>
2024-07-04 01:03:56 +0200

    filtre store with succursale

--- apps/backend/src/modules/store/controller/store.controller.ts
+++ apps/backend/src/modules/store/controller/store.controller.ts
@@ -1,17 +1,24 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { CrudController } from '@uni/crud';
 import { StoreService } from '../service/store.service';
 import { StoreEntity } from '../entities/store.entity';
 import { StoreDto } from '../dto/store-dto';
+import { AuthGuard } from '@uni/modules/auth/guards/auth.guard';
 
 @Controller()
 export class StoreController extends CrudController<StoreEntity> {
     constructor(private storeService: StoreService) {
         super(storeService)
     }
-
+    @UseGuards(AuthGuard)
     @Post()
-    async createStor(@Body() store: StoreDto):Promise<StoreEntity>{
+    async createStore(@Body() store: StoreDto):Promise<StoreEntity>{
         return await this.storeService.createStore(store);
+    }
+
+    @UseGuards(AuthGuard)
+    @Get()
+    async findStore():Promise<StoreEntity[]>{
+        return await this.storeService.findStore();
     }
 }
